953cdda52345212242978531d917a9c5f9932920
KiruthigaRajendranSE <62032172+KiruthigaRajendranSE@users.noreply.github.com>
2020-04-15 23:27:10 -0700

    Create the Category Entity and Repository

--- src/main/java/com/pluralsight/blog/data/CategoryRepository.java
+++ src/main/java/com/pluralsight/blog/data/CategoryRepository.java
@@ -1,21 +1,21 @@
 package com.pluralsight.blog.data;
 
-import com.pluralsight.blog.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
-import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pluralsight.blog.model.Category;
+import com.pluralsight.blog.model.Post;
 
 
-@Component
-public class CategoryRepository {
+/*
+ * @Component public class CategoryRepository {
+ * 
+ * public List<Category> findAll() { return null; }
+ * 
+ * public Optional<Category> findById(Long id) { return null; } }
+ */
 
-    public List<Category> findAll() {
-        return null;
-    }
-
-    public Optional<Category> findById(Long id) {
-        return null;
-    }
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	List<Post> findByCategory(Category category);
 }
